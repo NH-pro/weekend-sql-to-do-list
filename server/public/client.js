c70e605c2f46b72096d6e3bfb1e624063e2a5b0f
Neil Hanson <pro.neilhanson@gmail.com>
2022-05-27 20:08:54 -0500

    added an event listener for when add task btn is clicked

--- server/public/client.js
+++ server/public/client.js
@@ -1,5 +1,16 @@
 $(document).ready(readyHandlers);
 
 function readyHandlers() {
-    console.log(`--- In readyHandlers ---`);
+    console.log(`--- In readyHandlers function ---`);
+    // connection test
+
+    $('#add_task_btn').on('click', addTask);
+    // event listener when the 'add_task_btn' is clicked, run 'addTask' function
+}
+
+function addTask() {
+    console.log(`--- In addTask function ---`);
+    // connection test
+
+    
 }
