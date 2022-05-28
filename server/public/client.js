9a1feb1ec0c2f769c80fa71b31ec49f0d7f000dd
Neil Hanson <pro.neilhanson@gmail.com>
2022-05-28 09:24:45 -0500

    changed plans and focused on getting DB displayed on DOM to work

--- server/public/client.js
+++ server/public/client.js
@@ -6,6 +6,45 @@ function readyHandlers() {
 
     $('#add_task_btn').on('click', addTask);
     // event listener when the 'add_task_btn' is clicked, run 'addTask' function
+
+    $('#task_display_container').on('click', '.delete_btn', deleteTask)
+
+    getTasks();
+}
+
+function getTasks() {
+    console.log(`--- In getTasks function ---`);
+    //connection test
+
+    $.ajax({
+        method: 'GET',
+        url: '/tasks'
+    })
+    .then((response) => {
+        console.log(`getTasks function Success!`, response);
+        for(let item of response) {
+            $('#task_display_container').append(`
+            <tr>
+                <td>${item.task}</td>
+                <td>${item.completed}</td>
+                <td>
+                    <button class= "delete_btn">Delete</button>
+                </td>
+                <td>
+                    <button class= "complete_btn">Complete</button>
+                </td>
+            </tr>
+            `);
+        };
+    })
+    .catch((err) => {
+        console.log(`getTasks function Failed!`, err);
+    })
+}
+
+function deleteTask() {
+    console.log(`--- In deleteTask funciton ---`);
+    // connnection test
 }
 
 function addTask() {
@@ -13,12 +52,6 @@ function addTask() {
     // connection test
 
     let taskInput = $('#task_input').val();
-
-    $('#task_display_container').append(`
-        <tr>
-            <td>${taskInput}</td>
-        </tr>
-    `);
 
     $('#task_input').val('');
     // empty 'task_input
